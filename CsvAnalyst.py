7f2c752dff316330424852cecab9e971da34c400
Litobor <bar_chiik@mail.ru>
2022-11-01 15:19:42 +0300

    Добавление функции поиска имени

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -48,7 +48,7 @@ def do_dialog():
 # Обработка csv файла при помощи pandas
 
 def pandas_read_csv(file_name):
-    df = pd.read_csv(file_name, header=None, sep=';')
+    df = pd.read_csv(file_name, header=[0], sep=';')
     cnt_rows = df.shape[0]
     cnt_columns = df.shape[1]
     label_11['text'] = cnt_rows
@@ -63,16 +63,29 @@ def get_column(df, column_ix):
     for i in range(cnt_rows):
         lst.append(df.iat[i,column_ix])
     return lst
-    
+
+# Если в этом поле имя, пусть вернет True
+
+def meet_name(field):
+    checkfor = ['Вера', 'Анатолий', 'Мария', 'Артём','Алексей',
+        'Валерия','Наталья','Оксана','Галина','Марина',
+        'Вероника','Виталий','Борис','Диана','Ева']
+    for s in checkfor:
+        if s in str(field): # Нашлось!
+            return True
+    # Ничего не совпало
+    return False
+
 # Обработчик нажатия кнопки
 
 def process_button():
     file_name = do_dialog()
     label_01['text'] = file_name
     df = pandas_read_csv(file_name)
-    lst = get_column(df,0)
+    lst = get_column(df,2)
     for item in lst:
-        output_text.insert(tk.END, str(item)+os.linesep)
+        output_text.insert(tk.END, str(item) + '    ' 
+            + str(meet_name(item))+os.linesep)
     mb.showinfo(title=None, message="Готово")
 
 # Создание кнопки
