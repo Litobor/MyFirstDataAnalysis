cafe7084a55f36ec58f07aff517d318634730777
Litobor <bar_chiik@mail.ru>
2022-11-02 16:15:22 +0300

    Добавление процента совпадения

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -86,10 +86,11 @@ def list_meet_name(fields_list):
         if meet_name(list_item):
             counter_meet += 1
     # Конец подсчета
-    if counter_meet / counter_total > 0.1:
-        return True
+    ratio = counter_meet / counter_total 
+    if ratio > 0.1:
+        return True, ratio
     # Не набралось нужного количества совпадений
-    return False
+    return False, ratio
 
 # Пройти все стобцы
 
@@ -97,20 +98,21 @@ def check_all_columns(df):
     columns_cnt = df.shape[1]
     for i in range(columns_cnt): # От 0 до columns_cnt - 1
         lst = get_column(df, i)
-        if list_meet_name(lst):
-            output_text.insert(tk.END,"В столбце " + str(i+1)
-            + " предположительно содержится имя." + os.linesep)
+        result = list_meet_name(lst)
+        if result[0]:
+            output_text.insert(tk.END, "В столбце " + str(i+1)
+                + " предположительно содержится имя " + os.linesep)
+            output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result[1]*100)
+                + "%." + os.linesep)
         else:
-            output_text.insert(tk.END,"Предположений для столбца " + str(i+1)
-            + " не найдено." + os.linesep)
-
+            output_text.insert(tk.END, "Предположений для столбца " + str(i+1)
+                + " не найденою" + os.linesep)
 # Обработчик нажатия кнопки
 
 def process_button():
     file_name = do_dialog()
     label_01['text'] = file_name
     df = pandas_read_csv(file_name)
-    lst = get_column(df, 2)
     check_all_columns(df)
     mb.showinfo(title=None, message="Готово")
 
